61c8aa5e9facdd4445b8c294ceff16038544df9d
rafaeldoering <rafael.doering.soares@usp.br>
2025-12-06 12:00:23 +0000

    Add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Workflow Manager API')
+    .setDescription('The Workflow Manager API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.port ?? 3000);
 }
 void bootstrap();
